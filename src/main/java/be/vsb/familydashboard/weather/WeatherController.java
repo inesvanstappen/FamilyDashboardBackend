000b905750711e809e89974c88b13d87638076ce
Ines Van Stappen <ines.vanstappen@gmail.com>
2025-08-08 09:04:42 +0200

    Enable Cross-Origin Resource Sharing (CORS) for `WeekMenuController`, `WeatherController`, and `ToDoController`.

--- src/main/java/be/vsb/familydashboard/weather/WeatherController.java
+++ src/main/java/be/vsb/familydashboard/weather/WeatherController.java
@@ -1,9 +1,11 @@
 package be.vsb.familydashboard.weather;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/weather")
 public class WeatherController {
